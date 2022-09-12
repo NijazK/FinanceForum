ec12fed769d7fa3b1eaec54016a2d7830ccba8bc
Nijaz Kovacevic <nijaz.kovacevic@snhu.edu>
2022-09-12 11:02:59 -0700

    Initial commit

---
+++ Blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Post, Reply, Profile
+
+admin.site.register(Post)
+admin.site.register(Reply)
+admin.site.register(Profile)
